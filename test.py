5699f2fd91265159d9f2b8b557754f4591193d46
LiShaokun <100552553+CVAE1@users.noreply.github.com>
2024-05-30 15:03:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ from unsloth import FastLanguageModel
 from transformers import TextStreamer
 
 
+# 加载微调后的模型
 max_seq_length = 2048 # Supports RoPE Scaling interally, so choose any!
-
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = "lora_model",
     max_seq_length = max_seq_length,
     dtype = None,
     load_in_4bit = True,
 )
+FastLanguageModel.for_inference(model)
 
+# 定义 Prompt 模版
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
@@ -20,7 +22,7 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 ### Response:
 {}"""
 
-FastLanguageModel.for_inference(model)
+# 预处理 Prompt
 inputs = tokenizer(
 [
     alpaca_prompt.format(
@@ -30,8 +32,10 @@ inputs = tokenizer(
     )
 ], return_tensors = "pt").to("cuda")
 
+# 模型输出 token
 text_streamer = TextStreamer(tokenizer)
 token_ids = model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128).squeeze(0)
 
+# token 解码成文本
 # results = tokenizer.decode(token_ids)
 # print(results)
